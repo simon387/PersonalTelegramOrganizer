7d18f5760b349e13ee1083550ce4c18adef0d7a1
simone.celia <simone.celia@eng.it>
2023-04-12 22:06:27 +0200

    hello_world

---
+++ constants.py
@@ -0,0 +1,20 @@
+import configparser
+import logging
+
+config = configparser.RawConfigParser()
+config.read("config.properties")
+# application's secrets
+SECRETS = "secrets"
+API_ID = config.get(SECRETS, "api.id")
+API_HASH = config.get(SECRETS, "api.hash")
+# application's settings
+APPLICATION = "application"
+case = config.get(APPLICATION, "log.level")
+if case == "info":
+	LOG_LEVEL = logging.INFO
+elif case == "debug":
+	LOG_LEVEL = logging.DEBUG
+elif case == "error":
+	LOG_LEVEL = logging.ERROR
+else:
+	LOG_LEVEL = logging.DEBUG
