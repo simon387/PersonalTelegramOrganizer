3e8a3d176ae24437f3e67588ec2a06b1baa98751
simone.celia <simone.celia@eng.it>
2023-12-14 10:38:18 +0100

    1.2.4

--- PersonalTelegramOrganizer.py
+++ PersonalTelegramOrganizer.py
@@ -35,29 +35,29 @@ async def on_message_set_it_as_read(client, message):
 	log.info("|")
 	channel = message.chat.username
 	log.info(f"Got message from this channel: {channel}")
-	if channel in blacklist:
-		log.info(f"Channel {channel} found in the blacklist! Setting it as read!")
-		log.debug(f"{client.APP_VERSION} - Setting channels {channel} as read! message = {message}")
-		await app.read_chat_history(channel)
-
-		# START topic's management - Following code block is just for topic's chat
-		top = message.reply_to_top_message_id
-		if top:
-			topic_id = top
-		else:
-			topic_id = message.reply_to_message_id
-		resolved_peer = await client.resolve_peer(peer_id=message.chat.id)
-		log.debug(f"resolved_peer= {resolved_peer}")
-		log.debug(f"message.id= {message.id}")
-		try:
-			await client.invoke(ReadDiscussion(peer=resolved_peer, msg_id=topic_id, read_max_id=2 ** 31 - 1))
-		except BadRequest as e:
-			log.error(f"Telegram API error: {e}")
-		except Exception as e:
-			log.error(f"Telegram error: {e}. Ignore this if {channel} doesn't have topics!")
-		# END topic's management
-	else:
+	if channel not in blacklist:
 		log.info(f"Channel {channel} NOT found in the blacklist! Nothing to do...")
+		return
+	# channel is in the blacklist
+	log.info(f"Channel {channel} found in the blacklist! Setting it as read!")
+	log.debug(f"{client.APP_VERSION} - Setting channels {channel} as read! message = {message}")
+	await app.read_chat_history(channel)  # it fails if is a topic's chat
+	# START topic's management - Following code block is just for topic's chat
+	top = message.reply_to_top_message_id
+	if top:
+		topic_id = top
+	else:
+		topic_id = message.reply_to_message_id
+	resolved_peer = await client.resolve_peer(peer_id=message.chat.id)
+	log.debug(f"resolved_peer= {resolved_peer}")
+	log.debug(f"message.id= {message.id}")
+	try:
+		await client.invoke(ReadDiscussion(peer=resolved_peer, msg_id=topic_id, read_max_id=2 ** 31 - 1))
+	except BadRequest as e:
+		log.error(f"Telegram API error: {e}")
+	except Exception as e:
+		log.error(f"Telegram error: {e}. Ignore this if {channel} doesn't have topics!")
+	# END topic's management
 
 
 log.info(f'Starting PersonalTelegramOrganizer, {get_version()}')
